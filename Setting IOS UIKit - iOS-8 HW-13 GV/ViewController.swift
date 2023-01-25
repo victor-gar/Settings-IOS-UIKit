1a66b70ecdc60497a461fdd36d58d11356a2c119
Garitskiy-Victor <dinomc4711@gmail.com>
2023-01-25 13:58:43 +0600

    Initial Commit

---
+++ Setting IOS UIKit - iOS-8 HW-13 GV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Setting IOS UIKit - iOS-8 HW-13 GV
+//
+//  Created by Victor Garitskyu on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
